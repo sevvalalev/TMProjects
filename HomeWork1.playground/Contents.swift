4940edfa90d1013de0592e39384ef18386304276
sevvalalev <sevvalalevis@gmail.com>
2023-01-04 13:21:51 +0300

    Update

--- HomeWork1.playground/Contents.swift
+++ HomeWork1.playground/Contents.swift
@@ -1,144 +1,144 @@
 import UIKit
 
 var sehir: String = "Ankara"
-print(sehir)
+print("Şehir adı: \(sehir)")
 
 var ulke: String = "Türkiye"
-print(ulke)
+print("Ülke adı : \(ulke)")
 
 var telefon: String = "05053474738"
-print(telefon)
+print("Telefon : \(telefon)")
 
 var postaKodu: String = "02000"
-print(postaKodu)
+print("Posta Kodu : \(postaKodu)")
 
 var email: String = "sevvalalevis@gmail.com"
-print(email)
+print("Email : \(email)")
 
 var meslek: String = "Bilgisayar Mühendisi"
-print(meslek)
+print("Meslek : \(meslek)")
 
 var stokMiktarı: Int = 213
-print(stokMiktarı)
+print("Stok Miktarı: \(stokMiktarı)")
 
 var musteriAdı: String = "Şevval"
-print(musteriAdı)
+print("Müşteri Adı : \(musteriAdı)")
 
 var bakiye: Int = 1500
-print(bakiye)
+print("Bakiye : \(bakiye)")
 
 let date = Calendar.current
 if let dogumGunu = date.date(byAdding: .year, value: -23, to: Date()) {
-    print(dogumGunu)
+    print("Doğum günü : \(dogumGunu)")
 }
 
 var maas: Int = 15000
-print(maas)
+print("Maaş : \(maas)")
 
 var medeniDurum: String = "Bekar"
-print(medeniDurum)
+print("Medeni durum : \(medeniDurum)")
 
 var urunYorum: String = "Nice"
-print(urunYorum)
+print("Ürün yorum : \(urunYorum)")
 
 var odemeTarihi = Date()
-print(odemeTarihi)
+print("Ödeme tarihi : \(odemeTarihi)")
 
 var odeme: Double = 480.56
-print(odeme)
+print("Ödeme : \(odeme)")
 
 var siparisAdeti: Int = 16
-print(siparisAdeti)
+print("Sipariş adeti : \(siparisAdeti)")
 
 var arabaModeli: String = "Mercedes"
-print(arabaModeli)
+print("Araba modeli : \(arabaModeli)")
 
 var kitapAdı: String = "Ben, Kirke"
-print(kitapAdı)
+print("Kitap adı : \(kitapAdı)")
 
 if let yayınlamaTarihi = date.date(byAdding: .year, value: -15, to: Date()) {
-    print(yayınlamaTarihi)
+    print("Yayınlama tarihi : \(yayınlamaTarihi)")
 }
 
 var indirimMiktarı: Double = 0.3
-print(indirimMiktarı)
+print("İndirim miktarı : \(indirimMiktarı)")
 
 var odaSayisi: Int = 13
-print(odaSayisi)
+print("Oda sayısı : \(odaSayisi)")
 
 var enlem: Double = 2122222.8
-print(enlem)
+print("Enlem : \(enlem)")
 
 var boylam: Double = 3222333.8
-print(boylam)
+print("Boylam : \(boylam)")
 
 var urunAdı: String = "Macbook Air"
-print(urunAdı)
+print("Ürün adı : \(urunAdı)")
 
 var yemekFiyatı: Double = 132.56
-print(yemekFiyatı)
+print("Yemek fiyatı : \(yemekFiyatı)")
 
 var marka: String = "Apple"
-print(marka)
+print("Marka : \(marka)")
 
 var muzikAdı: String = "Before You"
-print(muzikAdı)
+print("Müzik adı: \(muzikAdı)")
 
 var videoSuresi: Double = 25.42
-print(videoSuresi)
+print("Video süresi : \(videoSuresi)")
 
 var urunPuanı: Double = 4.75
-print(urunPuanı)
+print("Ürün puanı : \(urunPuanı)")
 
 var resimAdı: String = "Nature"
-print(resimAdı)
+print("Resim adı: \(resimAdı)")
 
 var dosyaFormatı: String = ".jpeg"
-print(dosyaFormatı)
+print("Dosya formatı : \(dosyaFormatı)")
 
 var renk: UIColor = .blue
-print(renk)
+print("Renk : \(renk)")
 
 var renkKodu: String = "#BCB8BE"
-print(renkKodu)
+print("Renk kodu : \(renkKodu)")
 
 var telefonModeli: String = "Iphone 11"
-print(telefonModeli)
+print("Telefon modeli : \(telefonModeli)")
 
 var ekranBoyutu: Double = 6.06
-print(ekranBoyutu)
+print("Ekran boyutu : \(ekranBoyutu)")
 
 var agırlık: Double = 78.8
-print(agırlık)
+print("Ağırlık : \(agırlık)")
 
 var ulusalGun: String = "10 Kasım"
-print(ulusalGun)
+print("Ulusal gün : \(ulusalGun)")
 
 var tatilGunu = Date()
-print(tatilGunu)
+print("Tatil günü : \(tatilGunu)")
 
 var rezervasyonTarihi = Date()
-print(rezervasyonTarihi)
+print("Rezervasyon tarihi : \(rezervasyonTarihi)")
 
 var sokakaAdı: String = "Bahçeli"
-print(sokakaAdı)
+print("Sokak adı : \(sokakaAdı)")
 
 var otobusHattı: Int = 16
-print(otobusHattı)
+print("Otobüs hattı : \(otobusHattı)")
 
 var kalanDakika: Int = 13
-print(kalanDakika)
+print("Kalan dakika : \(kalanDakika)")
 
 var takipKodu: String = "1341238"
-print(takipKodu)
+print("Takip Kodu : \(takipKodu)")
 
 var kuponSuresi: String = "12 gün"
-print(kuponSuresi)
+print("Kupon süresi : \(kuponSuresi)")
 
 var kuponKodu: String = "KS30"
-print(kuponKodu)
+print("Kupon kodu : \(kuponKodu)")
 
 var faturaAdresi: String = "Ankara/Yenimahalle"
-print(faturaAdresi)
+print("Fatura Adresi : \(faturaAdresi)")
 
 
