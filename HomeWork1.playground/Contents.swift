87a1b649994fc450ae0658e31bab3a1f180865f4
sevvalalev <sevvalalevis@gmail.com>
2023-01-04 12:49:25 +0300

    HomeWork-1

---
+++ HomeWork1.playground/Contents.swift
@@ -0,0 +1,144 @@
+import UIKit
+
+var sehir: String = "Ankara"
+print(sehir)
+
+var ulke: String = "Türkiye"
+print(ulke)
+
+var telefon: String = "05053474738"
+print(telefon)
+
+var postaKodu: String = "02000"
+print(postaKodu)
+
+var email: String = "sevvalalevis@gmail.com"
+print(email)
+
+var meslek: String = "Bilgisayar Mühendisi"
+print(meslek)
+
+var stokMiktarı: Int = 213
+print(stokMiktarı)
+
+var musteriAdı: String = "Şevval"
+print(musteriAdı)
+
+var bakiye: Int = 1500
+print(bakiye)
+
+let date = Calendar.current
+if let dogumGunu = date.date(byAdding: .year, value: -23, to: Date()) {
+    print(dogumGunu)
+}
+
+var maas: Int = 15000
+print(maas)
+
+var medeniDurum: String = "Bekar"
+print(medeniDurum)
+
+var urunYorum: String = "Nice"
+print(urunYorum)
+
+var odemeTarihi = Date()
+print(odemeTarihi)
+
+var odeme: Double = 480.56
+print(odeme)
+
+var siparisAdeti: Int = 16
+print(siparisAdeti)
+
+var arabaModeli: String = "Mercedes"
+print(arabaModeli)
+
+var kitapAdı: String = "Ben, Kirke"
+print(kitapAdı)
+
+if let yayınlamaTarihi = date.date(byAdding: .year, value: -15, to: Date()) {
+    print(yayınlamaTarihi)
+}
+
+var indirimMiktarı: Double = 0.3
+print(indirimMiktarı)
+
+var odaSayisi: Int = 13
+print(odaSayisi)
+
+var enlem: Double = 2122222.8
+print(enlem)
+
+var boylam: Double = 3222333.8
+print(boylam)
+
+var urunAdı: String = "Macbook Air"
+print(urunAdı)
+
+var yemekFiyatı: Double = 132.56
+print(yemekFiyatı)
+
+var marka: String = "Apple"
+print(marka)
+
+var muzikAdı: String = "Before You"
+print(muzikAdı)
+
+var videoSuresi: Double = 25.42
+print(videoSuresi)
+
+var urunPuanı: Double = 4.75
+print(urunPuanı)
+
+var resimAdı: String = "Nature"
+print(resimAdı)
+
+var dosyaFormatı: String = ".jpeg"
+print(dosyaFormatı)
+
+var renk: UIColor = .blue
+print(renk)
+
+var renkKodu: String = "#BCB8BE"
+print(renkKodu)
+
+var telefonModeli: String = "Iphone 11"
+print(telefonModeli)
+
+var ekranBoyutu: Double = 6.06
+print(ekranBoyutu)
+
+var agırlık: Double = 78.8
+print(agırlık)
+
+var ulusalGun: String = "10 Kasım"
+print(ulusalGun)
+
+var tatilGunu = Date()
+print(tatilGunu)
+
+var rezervasyonTarihi = Date()
+print(rezervasyonTarihi)
+
+var sokakaAdı: String = "Bahçeli"
+print(sokakaAdı)
+
+var otobusHattı: Int = 16
+print(otobusHattı)
+
+var kalanDakika: Int = 13
+print(kalanDakika)
+
+var takipKodu: String = "1341238"
+print(takipKodu)
+
+var kuponSuresi: String = "12 gün"
+print(kuponSuresi)
+
+var kuponKodu: String = "KS30"
+print(kuponKodu)
+
+var faturaAdresi: String = "Ankara/Yenimahalle"
+print(faturaAdresi)
+
+
