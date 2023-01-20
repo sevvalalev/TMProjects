14ebe7c96bc6dce5d82dc4ecc538c23d9f146386
sevvalalev <sevvalalevis@gmail.com>
2023-01-21 00:03:07 +0300

    Calculator Update

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numLabel: UILabel!
+    
+    private var endTyping: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func calculationButtonTapped(_ sender: UIButton) {
+        
+        endTyping = true
+        
+    }
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+       
+        if let num = sender.titleLabel?.text {
+            if endTyping == true {
+                numLabel.text = num
+                endTyping = false
+            }else {
+                numLabel.text = numLabel.text! + num
+            }
+        }
+    }
+    
 
 }
 
