5b365b6e87a9758cc9d5ea61b01e22c783201984
sevvalalev <sevvalalevis@gmail.com>
2023-01-27 19:48:15 +0300

    Update

--- AppStoreDesign/AppStoreDesign/ViewController.swift
+++ AppStoreDesign/AppStoreDesign/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureTableView()
+        registerCell()
     }
+    
+    func registerCell() {
+        let nib = UINib(nibName: "GameCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: GameCell.identifier)
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: GameCell.identifier,for: indexPath) as? GameCell {
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
 }
 
